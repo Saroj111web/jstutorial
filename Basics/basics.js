bfd98237aa7c794561cc500e9fa5c68dd59bf22b
Saroj111web <khanalsaroj2002@gmail.com>
2024-08-24 05:09:55 +0000

    done

---
+++ Basics/basics.js
@@ -0,0 +1,5 @@
+const customerId=2213121
+let customerEmail="khanalsaroj2002@gmail.com"
+let customerPassword="abs123"
+let accountCity="Bengaluru"
+console.table([customerId],[customerEmail],[customerPassword],[accountCity])
