bc8490a43d451fd7e3264d80f1a1710448086949
Saroj111web <khanalsaroj2002@gmail.com>
2024-08-24 05:39:49 +0000

    yes move ahead

--- Basics/basics.js
+++ Basics/basics.js
@@ -1,5 +1,22 @@
-const customerId=2213121
-let customerEmail="khanalsaroj2002@gmail.com"
-let customerPassword="abs123"
-let accountCity="Bengaluru"
-console.table([customerId],[customerEmail],[customerPassword],[accountCity])
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+//because it is constant
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ Basics/datatype.js
@@ -0,0 +1,17 @@
+"use strict"
+//treat all js code as newer version
+//alert (3+3) we are using node js not browser
+console.log("saroj")
+let name="Saroj"
+let age=21
+let isloggedin=false
+let state;
+//number 2->53 
+//BigInt
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object`
