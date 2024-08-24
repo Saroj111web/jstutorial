de824f513d46f63b4d00932a96867a77fda33846
Saroj111web <khanalsaroj2002@gmail.com>
2024-08-24 06:19:39 +0000

    comparision

---
+++ Basics/comparision.js
@@ -0,0 +1,15 @@
+console.log(2>3);
+console.log(2>=3);
+console.log(2==3);
+console.log(2!=3);
+console.log(2<=3);
+
+//the below type of codes should be avoided
+console.log("2">1);
+console.log("02">1);
+console.log(null>0);
+console.log(null>=0);
+
+//strict check using "===" as it also checks data type
+console.log("2"===2)
+
